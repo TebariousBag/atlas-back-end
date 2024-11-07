6c13a0cc9d5ac2d373ecb3fc4bf23e61fa145e32
“Tristian <tristian.davis@atlasschool.com>
2024-11-07 21:19:15 +0000

    trying to find tasks in todods

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,5 +8,21 @@ def get_employee(employee_id):
     """ get data """
 
     """ get user info and todos info from the site provided"""
-    user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos = f"https://jsonplaceholder.typicode.com/todos/{employee_id}"
+    user_request = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    todos_request = requests.get(f"https://jsonplaceholder.typicode.com/todos/{employee_id}")
+    
+    """ status request 200 is success """
+    """ if not 200 then error """
+    if user_request.status_code != 200 or todos_request.status_code != 200:
+        print("error")
+        return
+    
+    """ parse json data and convert to python """
+    user_data = user_request.json()
+    todos_data = todos_request.json()
+
+    """ now get data from parsed data"""
+    """ use get to access keys, first we get 'name' """
+    EMPLOYEE_NAME = user_data.get("name")
+    NUMBER_OF_DONE_TASKS = 
+
